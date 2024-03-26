83ec447e99871ed98a5a18b2581dba37a987b5ca
David <david_gunawan_@hotmail.com>
2024-03-26 14:32:11 +0700

    Refactor PetAdoptionView, split  component code

--- cadoptio/PetAdoptionView.swift
+++ cadoptio/PetAdoptionView.swift
@@ -49,75 +49,95 @@ struct PetCard: View {
     let gender: String
     
     var body: some View {
-        HStack() {
-            Rectangle()
-                .frame(width: 100, height: 100)
-                .cornerRadius(8)
-                .overlay(
-                    Image("placeholder")
-                        .resizable()
-                        .scaledToFit()
-                )
-            VStack(alignment: .leading, spacing: 8) {
-                HStack {
-                    Text(name)
-                        .font(.headline)
-                    Spacer()
-                    Button(action: {}) {
-                        Image(systemName: "heart")
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .frame(width: 18, height: 18)
-                            .foregroundColor(.pink)
-                            .font(.title)
-                    }
-                }
-                Text(type)
-                    .font(.subheadline)
-                    .foregroundColor(.gray)
-                HStack {
-                    HStack {
-                        Image(systemName: "location.fill")
-                            .resizable()
-                            .foregroundColor(.orange)
-                            .aspectRatio(contentMode: .fit)
-                            .frame(width: 14, height: 14)
-                        Text("\(distance, specifier: "%.1f") km")
-                            .font(.system(size: 12, weight: .regular))
-                            .foregroundColor(.gray)
-                        Spacer()
-                    }
-                    HStack {
-                        Image(systemName: "scalemass.fill")
-                            .resizable()
-                            .foregroundColor(.orange)
-                            .aspectRatio(contentMode: .fit)
-                            .frame(width: 14, height: 14)
-                        Text("\(weight, specifier: "%.1f") kg")
-                            .font(.system(size: 12, weight: .regular))
-                            .foregroundColor(.gray)
-                        Spacer()
-                    }
-                    HStack {
-                        Image(systemName: "pawprint.fill")
-                            .resizable()
-                            .foregroundColor(.orange)
-                            .aspectRatio(contentMode: .fit)
-                            .frame(width: 14, height: 14)
-                        Text(gender)
-                            .font(.system(size: 12, weight: .regular))
-                            .foregroundColor(.gray)
-                        Spacer()
-                    }
-                }
-            }
-            .padding(.horizontal, 10.0)
+        HStack {
+            ImageView(imageName: "placeholder")
+                .background(Color(.white))
+            PetInfo(type: type, name: name, distance: distance, weight: weight, gender: gender)
+                .padding(.horizontal, 10.0)
         }
+        .frame(height: 120)
         .background(Color(.white))
-        .frame(width: .infinity, height: .infinity, alignment: .center)
         .cornerRadius(8)
     }
+}
+
+struct PetInfo: View {
+    let type: String
+    let name: String
+    let distance: Double
+    let weight: Double
+    let gender: String
     
+    var body: some View {
+        VStack(alignment: .leading, spacing: 8) {
+            HStack {
+                Text(name)
+                    .font(.headline)
+                Spacer()
+                Button(action: {}) {
+                    Image(systemName: "heart")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 18, height: 18)
+                        .foregroundColor(.pink)
+                        .font(.title)
+                }
+            }.padding(.trailing, 16)
+            Text(type)
+                .font(.system(size: 12, weight: .regular))
+                .foregroundColor(.gray)
+            Spacer()
+            HStack {
+                HStack {
+                    Image(systemName: "location.fill")
+                        .resizable()
+                        .foregroundColor(.orange)
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 14, height: 14)
+                    Text("\(distance, specifier: "%.1f") km")
+                        .font(.system(size: 12, weight: .regular))
+                        .foregroundColor(.gray)
+                    Spacer()
+                }
+                HStack {
+                    Image(systemName: "scalemass.fill")
+                        .resizable()
+                        .foregroundColor(.orange)
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 14, height: 14)
+                    Text("\(weight, specifier: "%.1f") kg")
+                        .font(.system(size: 12, weight: .regular))
+                        .foregroundColor(.gray)
+                    Spacer()
+                }
+                HStack {
+                    Image(systemName: "pawprint.fill")
+                        .resizable()
+                        .foregroundColor(.orange)
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 14, height: 14)
+                    Text(gender)
+                        .font(.system(size: 12, weight: .regular))
+                        .foregroundColor(.gray)
+                    Spacer()
+                }
+            }
+        }.padding(.vertical, 16.0)
+    }
+}
+
+struct ImageView: View {
+    let imageName: String
+    var body: some View {
+        Rectangle()
+            .foregroundColor(.clear)
+            .frame(width: 120, height: 120) // Set the frame here
+            .overlay(
+                Image(imageName)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+            )
+    }
 }
 
 struct PetAdoptionView_Previews: PreviewProvider {
