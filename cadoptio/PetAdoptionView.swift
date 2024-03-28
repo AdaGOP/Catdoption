fc6aba00e09c017a9757004c1f06b8af5d8fe1f2
David <david_gunawan_@hotmail.com>
2024-03-28 16:08:55 +0700

    Complete Project for Day 1

--- cadoptio/PetAdoptionView.swift
+++ cadoptio/PetAdoptionView.swift
@@ -14,6 +14,8 @@ struct PetAdoptionView: View {
     var body: some View {
         NavigationStack {
             VStack(alignment: .leading, spacing: 16) {
+                
+                // PET 1
                 HStack() {
                     Image("Leppy")
                         .resizable()
@@ -44,7 +46,6 @@ struct PetAdoptionView: View {
                                     .foregroundColor(.orange)
                                     .aspectRatio(contentMode: .fit)
                                     .frame(width: 14, height: 14)
-                                    .opacity(calculateDistanceOpacity(distance: 1.0))
                                 Text("1 km")
                                     .font(.system(size: 12, weight: .regular))
                                     .foregroundColor(.gray)
@@ -56,7 +57,6 @@ struct PetAdoptionView: View {
                                     .foregroundColor(.orange)
                                     .aspectRatio(contentMode: .fit)
                                     .frame(width: 14, height: 14)
-                                    .opacity(calculateWeightOpacity(weight: 3.0))
                                 Text("3 kg")
                                     .font(.system(size: 12, weight: .regular))
                                     .foregroundColor(.gray)
@@ -69,6 +69,207 @@ struct PetAdoptionView: View {
                                     .aspectRatio(contentMode: .fit)
                                     .frame(width: 14, height: 14)
                                 Text("Male")
+                                    .font(.system(size: 12, weight: .regular))
+                                    .foregroundColor(.gray)
+                                Spacer()
+                            }
+                        }
+                    }
+                    .padding(.horizontal, 10.0)
+                }
+                .background(Color(.white))
+                .frame(width: .infinity, height: .infinity, alignment: .center)
+                .cornerRadius(8)
+                .shadow(color: .black.opacity(0.2), radius: 4)
+                
+                // PET 2
+                HStack() {
+                    Image("Butet")
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 100, height: 100)
+                        .cornerRadius(8)
+                    VStack(alignment: .leading, spacing: 8) {
+                        HStack {
+                            Text("Butet")
+                                .font(.headline)
+                            Spacer()
+                            Button(action: {}) {
+                                Image(systemName: "heart")
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .frame(width: 18, height: 18)
+                                    .foregroundColor(.pink)
+                                    .font(.title)
+                            }
+                        }
+                        Text("Persian")
+                            .font(.subheadline)
+                            .foregroundColor(.gray)
+                        HStack {
+                            HStack {
+                                Image(systemName: "location.fill")
+                                    .resizable()
+                                    .foregroundColor(.orange)
+                                    .aspectRatio(contentMode: .fit)
+                                    .frame(width: 14, height: 14)
+                                Text("3,5 km")
+                                    .font(.system(size: 12, weight: .regular))
+                                    .foregroundColor(.gray)
+                                Spacer()
+                            }
+                            HStack {
+                                Image(systemName: "scalemass.fill")
+                                    .resizable()
+                                    .foregroundColor(.orange)
+                                    .aspectRatio(contentMode: .fit)
+                                    .frame(width: 14, height: 14)
+                                Text("5 kg")
+                                    .font(.system(size: 12, weight: .regular))
+                                    .foregroundColor(.gray)
+                                Spacer()
+                            }
+                            HStack {
+                                Image(systemName: "pawprint.fill")
+                                    .resizable()
+                                    .foregroundColor(.orange)
+                                    .aspectRatio(contentMode: .fit)
+                                    .frame(width: 14, height: 14)
+                                Text("Female")
+                                    .font(.system(size: 12, weight: .regular))
+                                    .foregroundColor(.gray)
+                                Spacer()
+                            }
+                        }
+                    }
+                    .padding(.horizontal, 10.0)
+                }
+                .background(Color(.white))
+                .frame(width: .infinity, height: .infinity, alignment: .center)
+                .cornerRadius(8)
+                .shadow(color: .black.opacity(0.2), radius: 4)
+                
+                // PET 3
+                HStack() {
+                    Image("Sky")
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 100, height: 100)
+                        .cornerRadius(8)
+                    VStack(alignment: .leading, spacing: 8) {
+                        HStack {
+                            Text("Sky")
+                                .font(.headline)
+                            Spacer()
+                            Button(action: {}) {
+                                Image(systemName: "heart")
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .frame(width: 18, height: 18)
+                                    .foregroundColor(.pink)
+                                    .font(.title)
+                            }
+                        }
+                        Text("Domestic")
+                            .font(.subheadline)
+                            .foregroundColor(.gray)
+                        HStack {
+                            HStack {
+                                Image(systemName: "location.fill")
+                                    .resizable()
+                                    .foregroundColor(.orange)
+                                    .aspectRatio(contentMode: .fit)
+                                    .frame(width: 14, height: 14)
+                                Text("8,5 km")
+                                    .font(.system(size: 12, weight: .regular))
+                                    .foregroundColor(.gray)
+                                Spacer()
+                            }
+                            HStack {
+                                Image(systemName: "scalemass.fill")
+                                    .resizable()
+                                    .foregroundColor(.orange)
+                                    .aspectRatio(contentMode: .fit)
+                                    .frame(width: 14, height: 14)
+                                Text("4,9 kg")
+                                    .font(.system(size: 12, weight: .regular))
+                                    .foregroundColor(.gray)
+                                Spacer()
+                            }
+                            HStack {
+                                Image(systemName: "pawprint.fill")
+                                    .resizable()
+                                    .foregroundColor(.orange)
+                                    .aspectRatio(contentMode: .fit)
+                                    .frame(width: 14, height: 14)
+                                Text("Female")
+                                    .font(.system(size: 12, weight: .regular))
+                                    .foregroundColor(.gray)
+                                Spacer()
+                            }
+                        }
+                    }
+                    .padding(.horizontal, 10.0)
+                }
+                .background(Color(.white))
+                .frame(width: .infinity, height: .infinity, alignment: .center)
+                .cornerRadius(8)
+                .shadow(color: .black.opacity(0.2), radius: 4)
+                
+                // PET 4
+                HStack() {
+                    Image("Kentang")
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 100, height: 100)
+                        .cornerRadius(8)
+                    VStack(alignment: .leading, spacing: 8) {
+                        HStack {
+                            Text("Kentang")
+                                .font(.headline)
+                            Spacer()
+                            Button(action: {}) {
+                                Image(systemName: "heart")
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .frame(width: 18, height: 18)
+                                    .foregroundColor(.pink)
+                                    .font(.title)
+                            }
+                        }
+                        Text("British Shorthair")
+                            .font(.subheadline)
+                            .foregroundColor(.gray)
+                        HStack {
+                            HStack {
+                                Image(systemName: "location.fill")
+                                    .resizable()
+                                    .foregroundColor(.orange)
+                                    .aspectRatio(contentMode: .fit)
+                                    .frame(width: 14, height: 14)
+                                Text("8,5 km")
+                                    .font(.system(size: 12, weight: .regular))
+                                    .foregroundColor(.gray)
+                                Spacer()
+                            }
+                            HStack {
+                                Image(systemName: "scalemass.fill")
+                                    .resizable()
+                                    .foregroundColor(.orange)
+                                    .aspectRatio(contentMode: .fit)
+                                    .frame(width: 14, height: 14)
+                                Text("4,9 kg")
+                                    .font(.system(size: 12, weight: .regular))
+                                    .foregroundColor(.gray)
+                                Spacer()
+                            }
+                            HStack {
+                                Image(systemName: "pawprint.fill")
+                                    .resizable()
+                                    .foregroundColor(.orange)
+                                    .aspectRatio(contentMode: .fit)
+                                    .frame(width: 14, height: 14)
+                                Text("Female")
                                     .font(.system(size: 12, weight: .regular))
                                     .foregroundColor(.gray)
                                 Spacer()
@@ -97,23 +298,6 @@ struct PetAdoptionView: View {
             .sheet(isPresented: $isFilterViewPresented) {
                 FilterView()
             }
-        }
-    }
-    
-    
-    func calculateWeightOpacity(weight: Double) -> Double {
-        if weight < 3.5 {
-            return 1.0
-        } else {
-            return 0.5
-        }
-    }
-    
-    func calculateDistanceOpacity(distance: Double) -> Double {
-        if distance < 5.0 {
-            return 1.0
-        } else {
-            return 0.5
         }
     }
 }
