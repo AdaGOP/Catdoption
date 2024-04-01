db18f0d28b3014ece4db5743424e115e906874db
David <david_gunawan_@hotmail.com>
2024-04-01 09:32:02 +0700

    Remove Functions

--- cadoptio/FilterView.swift
+++ cadoptio/FilterView.swift
@@ -6,13 +6,9 @@ struct FilterView: View {
     @State private var filtersSelectedCount = 0
     
     // State variables to store button tap count
-    @State private var breedTapCount = 0
-    @State private var genderTapCount = 0
-    @State private var ageTapCount = 0
-    @State private var pedigreeTapCount = 0
     
     func updateTotalFiltersCount() {
-        filtersSelectedCount = breedTapCount + genderTapCount + ageTapCount + pedigreeTapCount
+        
     }
     
     var body: some View {
@@ -25,22 +21,10 @@ struct FilterView: View {
             .frame(maxWidth: .infinity)
             
             Group {
-                FilterButton(segmentTitle: "Persian", itemTitle: "Breed", tapCount: $breedTapCount)
-                    .onChange(of: breedTapCount) {
-                        self.updateTotalFiltersCount()
-                    }
-                FilterButton(segmentTitle: "Male", itemTitle: "Gender", tapCount: $genderTapCount)
-                    .onChange(of: genderTapCount) {
-                        self.updateTotalFiltersCount()
-                    }
-                FilterButton(segmentTitle: "Adult", itemTitle: "Age", tapCount: $ageTapCount)
-                    .onChange(of: ageTapCount) {
-                        self.updateTotalFiltersCount()
-                    }
-                FilterButton(segmentTitle: "Champion", itemTitle: "Pedigree", tapCount: $pedigreeTapCount)
-                    .onChange(of: pedigreeTapCount) {
-                        self.updateTotalFiltersCount()
-                    }
+                FilterButton(segmentTitle: "Persian", itemTitle: "Breed")
+                FilterButton(segmentTitle: "Male", itemTitle: "Gender")
+                FilterButton(segmentTitle: "Adult", itemTitle: "Age")
+                FilterButton(segmentTitle: "Champion", itemTitle: "Pedigree")
                 // Add more buttons as necessary
             }
             .padding(.top, 12)
@@ -72,7 +56,6 @@ struct FilterView: View {
 struct FilterButton: View {
     var segmentTitle: String
     var itemTitle: String
-    @Binding var tapCount: Int
     
     var body: some View {
         VStack(alignment: .leading, spacing: 8) {
@@ -80,7 +63,7 @@ struct FilterButton: View {
                 .font(.headline)
             
             Button(action: {
-                self.tapCount += 1
+                
             }) {
                 Text(itemTitle)
                     .padding(12)
