c3f89b8a8e497a9dee846ce1b2c707a00bae4711
Khoirunnisa' Rizky Noor Fatimah <49296015+khoirunnisarizky@users.noreply.github.com>
2024-04-22 06:17:18 +0700

    add pet model and pet detail view

---
+++ cadoptio/PetDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  PetDetailView.swift
+//  cadoptio
+//
+//  Created by khoirunnisa' rizky noor fatimah on 19/04/24.
+//
+
+import SwiftUI
+
+struct PetDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PetDetailView()
+}
---
+++ cadoptio/PetModel.swift
@@ -0,0 +1,8 @@
+//
+//  PetModel.swift
+//  cadoptio
+//
+//  Created by khoirunnisa' rizky noor fatimah on 22/04/24.
+//
+
+import Foundation
