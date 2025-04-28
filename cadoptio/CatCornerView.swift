c10a487c95bbf325d7c54f3545906e5337c5e537
Khoirunnisa' Rizky Noor Fatimah <49296015+khoirunnisarizky@users.noreply.github.com>
2025-04-28 10:52:43 +0700

    add new view: CatCornerView for GA Platform in EC Cohort 8

---
+++ cadoptio/CatCornerView.swift
@@ -0,0 +1,18 @@
+//
+//  CatCornerView.swift
+//  cadoptio
+//
+//  Created by khoirunnisa' rizky noor fatimah on 28/04/25.
+//
+
+import SwiftUI
+
+struct CatCornerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CatCornerView()
+}
