a74bd87c33d13befc881fa6dfdf9e5c1045dd28d
Khoirunnisa' Rizky Noor Fatimah <49296015+khoirunnisarizky@users.noreply.github.com>
2025-05-04 14:30:54 +0700

    Add proxy platform configuration to handle different card layout

---
+++ cadoptio/Extension/Color+Ext.swift
@@ -0,0 +1,8 @@
+//
+//  Color+Ext.swift
+//  cadoptio
+//
+//  Created by khoirunnisa' rizky noor fatimah on 04/05/25.
+//
+
+import Foundation
---
+++ cadoptio/General/WidthThresholdReader.swift
@@ -0,0 +1,18 @@
+//
+//  WidthThresholdReader.swift
+//  cadoptio
+//
+//  Created by khoirunnisa' rizky noor fatimah on 04/05/25.
+//
+
+import SwiftUI
+
+struct WidthThresholdReader: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WidthThresholdReader()
+}
